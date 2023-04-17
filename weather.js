67e7f19f42a91c6bdc43c4ca70801476d6a480c7
Michael <mharwood13@gmail.com>
2023-04-17 17:30:47 -0400

    finished

--- weather.js
+++ weather.js
@@ -12,9 +12,9 @@ let weather = {
         document.querySelector(".city").innerText = "Weather in " + name;
         document.querySelector(".icon").src ="https://openweathermap.org/img/wn/" + icon + ".png";
         document.querySelector(".description").innerText = description;
-        document.querySelector(".temp").innerText = temp + "°F";
+        document.querySelector(".temp").innerText = Math.round(temp) + "°F";
         document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
-        document.querySelector(".wind").innerText = "Wind speed " + speed + "mph";
+        document.querySelector(".wind").innerText = "Wind speed " + Math.round(speed) + "mph";
         document.querySelector(".weather").classList.remove("loading");
         document.body.style.backgroundImage = "url('https://source.unsplash.com/random/?" + name +"')"
     },
